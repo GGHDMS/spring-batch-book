eca2adb23cafec29bbc8311ac7d46deb1d0444d3
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-05-30 15:59:30 +0900

    잡 파라미터 유효성 검증

--- ch04/src/main/java/education/ch04/jobs/HelloWorldJob.java
+++ ch04/src/main/java/education/ch04/jobs/HelloWorldJob.java
@@ -1,8 +1,11 @@
 package education.ch04.jobs;
 
+import education.ch04.batch.ParameterValidator;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
@@ -15,6 +18,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class HelloWorldJob {
 
@@ -25,16 +30,37 @@ public class HelloWorldJob {
     private PlatformTransactionManager transactionManager;
 
     @Bean
-    public Step step() {
-        return new StepBuilder("step1", jobRepository)
-                .tasklet(helloWorldTasklet(null), transactionManager)
-                .build();
+    public CompositeJobParametersValidator validator() {
+        CompositeJobParametersValidator validator =
+                new CompositeJobParametersValidator();
+
+        DefaultJobParametersValidator defaultJobParametersValidator =
+                new DefaultJobParametersValidator(
+                        new String[]{"fileName"},
+                        new String[]{"name", "currentDate"});
+
+        defaultJobParametersValidator.afterPropertiesSet();
+
+        validator.setValidators(
+                Arrays.asList(new ParameterValidator(),
+                        defaultJobParametersValidator));
+
+        return validator;
     }
+
 
     @Bean
     public Job job() {
         return new JobBuilder("basicJob", jobRepository)
                 .start(step())
+                .validator(validator())
+                .build();
+    }
+
+    @Bean
+    public Step step() {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet(helloWorldTasklet(null, null), transactionManager)
                 .build();
     }
 
@@ -52,11 +78,19 @@ public class HelloWorldJob {
     }
 */
 
-    @Bean
     @StepScope
-    public Tasklet helloWorldTasklet(@Value("#{jobParameters['name']}") String name) {
+    @Bean
+    public Tasklet helloWorldTasklet(
+            @Value("#{jobParameters['name']}") String name,
+            @Value("#{jobParameters['fileName']}") String fileName) {
+
         return (contribution, chunkContext) -> {
-            System.out.println(String.format("Hello, %s!", name));
+
+            System.out.println(
+                    String.format("Hello, %s!", name));
+            System.out.println(
+                    String.format("fileName = %s", fileName));
+
             return RepeatStatus.FINISHED;
         };
     }
