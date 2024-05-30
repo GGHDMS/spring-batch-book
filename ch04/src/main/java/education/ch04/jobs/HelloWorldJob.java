fed815eacca06489177bff7fe3223dd03ae650dd
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-05-30 15:47:20 +0900

    잡 파라미터 접근

--- ch04/src/main/java/education/ch04/jobs/HelloWorldJob.java
+++ ch04/src/main/java/education/ch04/jobs/HelloWorldJob.java
@@ -1,12 +1,15 @@
-package education.ch04;
+package education.ch04.jobs;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -24,10 +27,8 @@ public class HelloWorldJob {
     @Bean
     public Step step() {
         return new StepBuilder("step1", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("Hello, World!");
-                    return RepeatStatus.FINISHED;
-                }, transactionManager).build();
+                .tasklet(helloWorldTasklet(null), transactionManager)
+                .build();
     }
 
     @Bean
@@ -35,6 +36,29 @@ public class HelloWorldJob {
         return new JobBuilder("basicJob", jobRepository)
                 .start(step())
                 .build();
+    }
+
+/*
+    @Bean
+    public Tasklet helloWorldTasklet() {
+        return (contribution, chunkContext) -> {
+            String name = (String) chunkContext.getStepContext()
+                    .getJobParameters()
+                    .get("name");
+
+            System.out.println(String.format("Hello, %s!", name));
+            return RepeatStatus.FINISHED;
+        };
+    }
+*/
+
+    @Bean
+    @StepScope
+    public Tasklet helloWorldTasklet(@Value("#{jobParameters['name']}") String name) {
+        return (contribution, chunkContext) -> {
+            System.out.println(String.format("Hello, %s!", name));
+            return RepeatStatus.FINISHED;
+        };
     }
 
     public static void main(String[] args) {
