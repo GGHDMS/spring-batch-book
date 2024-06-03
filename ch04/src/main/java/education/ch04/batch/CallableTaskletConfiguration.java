de5118406187329a3ea40d8a10aa1a152705df2f
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-06-03 10:18:10 +0900

    CallableTaskletAdapter

---
+++ ch04/src/main/java/education/ch04/batch/CallableTaskletConfiguration.java
@@ -0,0 +1,63 @@
+package education.ch04.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.CallableTaskletAdapter;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.concurrent.Callable;
+
+@SpringBootApplication
+public class CallableTaskletConfiguration {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    @Bean
+    public Job callableJob() {
+        return new JobBuilder("callableJob", jobRepository)
+                .start(callableStep())
+                .build();
+    }
+
+    @Bean
+    public Step callableStep() {
+        return new StepBuilder("callableStep", jobRepository)
+                .tasklet(tasklet(), transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Callable<RepeatStatus> callableObject() {
+        return () -> {
+            System.out.println("This was executed in another thread");
+
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+	@Bean
+	public CallableTaskletAdapter tasklet() {
+		CallableTaskletAdapter callableTaskletAdapter =
+				new CallableTaskletAdapter();
+
+		callableTaskletAdapter.setCallable(callableObject());
+
+		return callableTaskletAdapter;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallableTaskletConfiguration.class, args);
+	}
+}
--- ch04/src/main/java/education/ch04/jobs/BatchConfiguration.java
+++ ch04/src/main/java/education/ch04/jobs/BatchConfiguration.java
@@ -14,11 +14,10 @@ import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.PlatformTransactionManager;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class BatchConfiguration {
 
     @Autowired
